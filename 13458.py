a927602940530761b190157d269c31fd59999287
limminjeong98 <limminjeong98@naver.com>
2021-10-03 15:32:44 +0900

    시험감독

--- 13458.py
+++ 13458.py
@@ -1,6 +1,5 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
 b, c = map(int, input().split())
 answer = 0
 for i in range(n):
@@ -11,5 +10,4 @@ for i in range(n):
     if (arr[i] - b) % c != 0:
         answer += 1
     answer += (arr[i] - b) // c
-
 print(answer)
