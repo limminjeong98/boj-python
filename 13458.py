490ff066bc1e276a307c4687ed0be3b3b2ac8fde
limminjeong98 <limminjeong98@naver.com>
2021-10-03 15:31:24 +0900

    10/03

---
+++ 13458.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+b, c = map(int, input().split())
+answer = 0
+for i in range(n):
+    if arr[i] <= b:
+        answer += 1
+        continue
+    answer += 1
+    if (arr[i] - b) % c != 0:
+        answer += 1
+    answer += (arr[i] - b) // c
+
+print(answer)
