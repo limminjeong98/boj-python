165087f4ed7eb23be5cb2469f7e0a9c9839b3725
limminjeong98 <limminjeong98@naver.com>
2021-10-15 21:36:22 +0900

    컨베이어 벨트 위의 로봇

--- 20055.py
+++ 20055.py
@@ -1,9 +1,11 @@
+
+# 20055 컨베이어 벨트 위의 로봇
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
-a = deque(map(int, input().split()))
+n, k = map(int, input().rstrip().split())
+a = deque(map(int, input().rstrip().split()))
 robot = deque([False] * n)
 cnt = 0
 
@@ -11,21 +13,26 @@ while True:
     cnt += 1
     a.rotate(1)
     robot.rotate(1)
-    robot[n-1] = False
+    # 내리는 위치에 도달하면 즉시 내린다
+    robot[n - 1] = False
     # 올라가거나 이동할때만 내구도가 감소
     # 내려갈 때는 내구도가 감소하지 않음
-    for i in range(n-2, -1, -1):
+    for i in range(n - 2, -1, -1):
         if robot[i] == True:
-            if not robot[i+1] and a[i+1] > 0:
+            # 칸에 로봇이 없고 내구도가 0보다 커야 함
+            if not robot[i + 1] and a[i + 1] > 0:
                 robot[i] = False
-                robot[i+1] = True
-                a[i+1] -= 1
-    if robot[n-1]:
-        robot[n-1] = False
+                robot[i + 1] = True
+                a[i + 1] -= 1
+    # 언제든지 로봇이 내리는 위치에 도달하면 내린다
+    # n - 2에서 n - 1로 왔으면 즉시 내린다
+    if robot[n - 1]:
+        robot[n - 1] = False
         
     if not robot[0] and a[0] > 0:
         robot[0] = True
         a[0] -= 1
+    # 내구도가 0인 칸의 개수가 k개 이상이라면 종료
     if a.count(0) >= k:
         print(cnt)
         break
